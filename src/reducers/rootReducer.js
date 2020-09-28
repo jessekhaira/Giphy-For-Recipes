2ad9286b3a187f1ae46aeeb4706b61c27656197b
13jk59 <jesse.khaira10@gmail.com>
2020-09-28 15:19:28 -0600

    state finalized

--- src/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-/* State structure going to look like:
-
-{   currPage: home/favourites,
-    postsByPage: {
-        home: {
-            
-        }
-    }
-}
--- src/reduxConstants.js
+++ src/reduxConstants.js
@@ -9,3 +9,22 @@ export const ADD_TO_FAVOURITES = "ADD_TO_FAVOURITES";
 
 // removing post removes it from favourites
 export const REMOVE_FROM_FAVOURITES = "REMOVE_FROM_FAVOURITES"; 
+
+// Init state & state constants 
+export const HOME = "HOME";
+export const FAVOURITES = "FAVOURITES";
+export const FAVOURITES_HASH_SET = new Set();
+export const ITEMS_HOME = []; 
+
+export const INIT_STATE = {
+    HOME: {
+        isFetching: true,
+        lastUpdated: null,
+        items: ITEMS_HOME
+    },
+
+    FAVOURITES: {
+        posts: FAVOURITES_HASH_SET
+    }
+}
+
