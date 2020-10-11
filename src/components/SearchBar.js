9f9d933e5888e1f17a2ef84be800abf4a9751c0b
13jk59 <jesse.khaira10@gmail.com>
2020-10-11 10:50:28 -0600

    Made SearchBar a class component because it needs methods

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-function SearchBar(props) {
-    return(
-        <div id = "search_bar">
-            <label for = 'search'></label>
-            <input type = "text" id = "search_query" name = "search" placeholder="Search for recipes..."></input>
-            <button id = "submit_search">Search</button>
-        </div>
-    )
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props); 
+    }
+    render() {
+        return(
+            <div id = "search_bar">
+                <label for = 'search'></label>
+                <input type = "text" id = "search_query" name = "search" placeholder="Search for recipes..."></input>
+                <button id = "submit_search">Search</button>
+            </div>
+        )
+    }
 }
 
 export default SearchBar; 
