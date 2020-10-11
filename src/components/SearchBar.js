6d414a4c89ed284153797b3a7ed40d55bc26b127
13jk59 <jesse.khaira10@gmail.com>
2020-10-11 11:02:05 -0600

    Added a back button to Search Bar for the Search functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,9 +7,12 @@ class SearchBar extends React.Component {
     render() {
         return(
             <div id = "search_bar">
-                <label for = 'search'></label>
-                <input type = "text" id = "search_query" name = "search" placeholder="Search for recipes..."></input>
-                <button id = "submit_search">Search</button>
+                <div id = "mainSearch">
+                    <label for = 'search'></label>
+                    <input type = "text" id = "search_query" name = "search" placeholder="Search for recipes..."></input>
+                    <button id = "submit_search">Search</button>
+                </div> 
+                <button id = "backOffSearch">Go Back</button>
             </div>
         )
     }
