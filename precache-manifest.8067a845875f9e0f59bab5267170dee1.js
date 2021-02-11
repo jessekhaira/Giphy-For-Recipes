23fde9467fad641b483effd57293d64bda8da685
13jk59 <jesse.khaira10@gmail.com>
2021-02-10 19:12:39 -0700

    Updates

---
+++ precache-manifest.8067a845875f9e0f59bab5267170dee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4214c39dac113d7f20f19d85a287b00",
+    "url": "/Giphy-For-Recipes/index.html"
+  },
+  {
+    "revision": "f3c272a74bbd3178f813",
+    "url": "/Giphy-For-Recipes/static/css/main.0127e73e.chunk.css"
+  },
+  {
+    "revision": "95a0bb3f72f48d25bf5a",
+    "url": "/Giphy-For-Recipes/static/js/2.a974f222.chunk.js"
+  },
+  {
+    "revision": "9f90cde4396c52b73cc0a6bcd8267b7b",
+    "url": "/Giphy-For-Recipes/static/js/2.a974f222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c272a74bbd3178f813",
+    "url": "/Giphy-For-Recipes/static/js/main.e13b8b62.chunk.js"
+  },
+  {
+    "revision": "6f9907b466d704771539",
+    "url": "/Giphy-For-Recipes/static/js/runtime-main.315b9a21.js"
+  }
+]);
